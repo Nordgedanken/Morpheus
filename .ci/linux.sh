7c7c96215548251c47f0f290f9531624a75f9e10
MTRNord <mtrnord1@gmail.com>
2017-12-05 21:23:25 +0100

    install missing package
    Signed-off-by: MTRNord <mtrnord1@gmail.com>

--- .ci/linux.sh
+++ .ci/linux.sh
@@ -11,7 +11,7 @@ du -sh $HOME/*
 export QT_QPA_PLATFORM=minimal
 
 #additional deps for multimedia
-sudo apt-get -y install libpulse-dev && sudo apt-get clean
+sudo apt-get -y install libpulse-dev libfontconfig && sudo apt-get clean
 
 #replace gcc4 with gcc5
 echo "deb http://ftp.us.debian.org/debian unstable main contrib non-free" | sudo tee --append /etc/apt/sources.list.d/unstable.list
