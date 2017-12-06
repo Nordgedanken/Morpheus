baf6014fd2edb07809ce3c637f654096a82bc12a
MTRNord <mtrnord1@gmail.com>
2017-12-06 19:44:14 +0100

    Use custom checkout script
    Signed-off-by: MTRNord <mtrnord1@gmail.com>

---
+++ .circleci/checkout.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+set -e
+
+# Workaround old docker images with incorrect $HOME
+# check https://github.com/docker/docker/issues/2968 for details
+if [ "${HOME}" = "/" ]
+then
+  export HOME=$(getent passwd $(id -un) | cut -d: -f6)
+fi
+
+if [ -e /home/user/work/src/github.com/Nordgedanken/Morpheus/.git ]
+then
+  cd /home/user/work/src/github.com/Nordgedanken/Morpheus/
+  git remote set-url origin "$CIRCLE_REPOSITORY_URL" || true
+else
+  mkdir -p /home/user/work/src/github.com/Nordgedanken/Morpheus/
+  cd /home/user/work/src/github.com/Nordgedanken/Morpheus/
+  git clone "$CIRCLE_REPOSITORY_URL" .
+fi
+
+if [ -n "$CIRCLE_TAG" ]
+then
+  git fetch --force origin "refs/tags/${CIRCLE_TAG}"
+else
+  git fetch --force origin "${CIRCLE_BRANCH}:remotes/origin/${CIRCLE_BRANCH}"
+fi
+
+
+if [ -n "$CIRCLE_TAG" ]
+then
+  git reset --hard "$CIRCLE_SHA1"
+  git checkout -q "$CIRCLE_TAG"
+elif [ -n "$CIRCLE_BRANCH" ]
+then
+  git reset --hard "$CIRCLE_SHA1"
+  git checkout -q -B "$CIRCLE_BRANCH"
+fi
+
+git reset --hard "$CIRCLE_SHA1"
