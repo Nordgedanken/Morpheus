eab200940294643d7378bad9d0c9abd2cdd0732a
MTRNord <mtrnord1@gmail.com>
2017-12-06 18:37:18 +0100

    Hopefully fix imports
    Signed-off-by: MTRNord <mtrnord1@gmail.com>

--- matrix/db/userDataDB_test.go
+++ matrix/db/userDataDB_test.go
@@ -5,7 +5,7 @@ import (
 	"path/filepath"
 
 	"github.com/dgraph-io/badger"
-	. "github.com/onsi/ginkgo"
+	"github.com/onsi/ginkgo"
 
 	//. "github.com/onsi/gomega"
 	"github.com/shibukawa/configdir"
@@ -56,6 +56,11 @@ func OpenUserDBTest() (db *badger.DB, err error) {
 	db = expDB
 	return
 }
+
+var Describe = ginkgo.Describe
+var Context = ginkgo.Context
+var It = ginkgo.It
+var Fail = ginkgo.Fail
 
 var _ = Describe("UserDataDB", func() {
 	var db *badger.DB
--- testSpec_test.go
+++ testSpec_test.go
@@ -3,9 +3,12 @@ package main_test
 import (
 	"fmt"
 
-	. "github.com/onsi/ginkgo"
+	"github.com/onsi/ginkgo"
 )
 
+var Describe = ginkgo.Describe
+var Context = ginkgo.Context
+var It = ginkgo.It
 var _ = Describe("TestSpec", func() {
 	Describe("Test", func() {
 		Context("opening", func() {
