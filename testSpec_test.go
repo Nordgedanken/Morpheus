8b54caedc2a6f5046af6abbe1817addde1b12da3
MTRNord <mtrnord1@gmail.com>
2017-12-06 18:40:36 +0100

    Run ginkgo nodot script
    Signed-off-by: MTRNord <mtrnord1@gmail.com>

--- testSpec_test.go
+++ testSpec_test.go
@@ -2,13 +2,8 @@ package main_test
 
 import (
 	"fmt"
-
-	"github.com/onsi/ginkgo"
 )
 
-var Describe = ginkgo.Describe
-var Context = ginkgo.Context
-var It = ginkgo.It
 var _ = Describe("TestSpec", func() {
 	Describe("Test", func() {
 		Context("opening", func() {
