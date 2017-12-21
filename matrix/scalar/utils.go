c66a2853a7362b417763ee55ca3294a808260915
MTRNord <mtrnord1@gmail.com>
2017-12-21 16:41:34 +0100

    Debug body
    Signed-off-by: MTRNord <mtrnord1@gmail.com>

--- matrix/scalar/utils.go
+++ matrix/scalar/utils.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 
 	"github.com/matrix-org/gomatrix"
+	log "github.com/sirupsen/logrus"
 )
 
 func MakeRequest(cli *gomatrix.Client, method string, httpURL string, reqBody string, resBody interface{}) ([]byte, error) {
@@ -22,6 +23,8 @@ func MakeRequest(cli *gomatrix.Client, method string, httpURL string, reqBody st
 	if err != nil {
 		return nil, err
 	}
+	ReqContents, _ := ioutil.ReadAll(req.Body)
+	log.Infoln(ReqContents)
 	req.Header.Set("Content-Type", "application/json")
 	res, err := cli.Client.Do(req)
 	if res != nil {
