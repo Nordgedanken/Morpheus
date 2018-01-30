804190c8be1eefba72f946cdf64c074c58af6a67
Marcel <MTRNord@users.noreply.github.com>
2018-01-30 17:25:32 +0100

    Make master buildable again

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ import (
 	"github.com/Nordgedanken/Morpheus/matrix/db"
 	"github.com/Nordgedanken/Morpheus/ui"
 	"github.com/dgraph-io/badger"
-	"github.com/matrix-org/dugong"
+	"github.com/Nordgedanken/dugong"
 	"github.com/matrix-org/gomatrix"
 	"github.com/shibukawa/configdir"
 	log "github.com/sirupsen/logrus"
